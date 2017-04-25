d3aaac38e8da661bcb00067afc83446b5d215801
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-04-26 01:03:54 +0800

    feat(Image): add cover image

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-snapshot';
 import App from './App';
-import './index.css';
+import 'normalize.css'
+import './style.js';
 
 render(
   <App />,
---
+++ src/style.js
@@ -0,0 +1,15 @@
+/* eslint no-unused-expressions:0 */
+
+import { injectGlobal } from 'styled-components';
+import cover from './statics/images/cover2.jpg';
+
+injectGlobal`
+  html {
+    font-size: 15px;
+    line-height: 1.5;
+  }
+
+  body {
+    margin: 0;
+  }
+`;
