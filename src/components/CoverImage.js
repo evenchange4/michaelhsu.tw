e1f39e9f62b567bd6fd7aa07cdf435af2724885a
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-04-29 15:13:35 +0800

    fix

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import ProgressiveImage from './ProgressiveImage';
 import cover from '../statics/images/cover.jpg';
 import cover_60 from '../statics/images/cover-60.jpg';
-import cover2 from '../statics/images/cover2.jpg';
-import cover2_60 from '../statics/images/cover2-60.jpg';
 
 const CoverImage = () => (
   <ProgressiveImage src={cover} placeholder={cover_60} />
