2811e18ee8b1f8a87cd5f9c6550b9dd87d091c02
Michael Hsu <evenchange4@gmail.com>
2017-05-02 16:35:32 +0800

    feat(coverimage): add medium-like lazy loading (#11)
    * feat(coverimage): add medium like lazy load
    
    * formated
    
    * fix
    
    * update isLoaded$
    
    * feat(responsive): introduce react-media
    
    * optimize bundle size
    
    * formated
    
    * feat(react-progressive-bg-image): extract to npm
    
    * docs(CHANGELOG): update

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -1,23 +1,48 @@
+import React from 'react';
 import styled from 'styled-components';
-import cover from '../statics/images/cover.jpg';
-import cover2 from '../statics/images/cover2.jpg';
+import Media from 'react-media';
+import ProgressiveImage from 'react-progressive-bg-image';
+import landscapeX60 from '../statics/images/landscapeX60.jpg';
+import landscape from '../statics/images/landscape.jpg';
+import portraitX60 from '../statics/images/portraitX60.jpg';
+import portrait from '../statics/images/portrait.jpg';
 
-const CoverImage = styled.div`
+/**
+ * matchMedia polyfill
+ * set default value to true
+ * ref: https://github.com/WickyNilliams/enquire.js/issues/82#issuecomment-26990494
+ * @author Michael Hsu
+ */
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: true,
+      addListener: function() {},
+      removeListener: function() {},
+    };
+  };
+
+const StyledProgressiveImage = styled(ProgressiveImage)`
   height: calc(100vh - 120px);
-  background-image: url(${cover2});
   background-color: aliceblue;
   background-size: cover;
   background-attachment: fixed;
   background-position-y: 70%;
   background-position-x: center;
-  background-repeat: no-repeat;
-
-  @media (min-width: 1000px) {
-    background-image: url(${cover});
-    background-size: cover;
-    background-position-y: -160px;
-  }
+  overflow: hidden;
 `;
+
+const CoverImage = () => (
+  <Media query={{ minWidth: 1000 }}>
+    {matches => (
+      <StyledProgressiveImage
+        src={matches ? portrait : landscape}
+        placeholder={matches ? portraitX60 : landscapeX60}
+      />
+    )}
+  </Media>
+);
 
 CoverImage.displayName = 'CoverImage';
 
