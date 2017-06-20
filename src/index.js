59b62ad606df43eb8c9b57763c55225955a48871
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-06-20 15:07:18 +0800

    feat(ga): add autotrack & ncu

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './pages/App';
 import 'normalize.css';
 import './style.js';
 import registerServiceWorker from './registerServiceWorker';
+import autotrack from './autotrack';
 
 render(<App />, document.getElementById('root'));
 registerServiceWorker();
+autotrack(process.env.REACT_APP_GA_ID);
