98940cdb872b91b632c7f3dd3375b2921d111d0b
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-08-08 21:01:02 +0800

    fix

--- src/utils/getFetchURL.js
+++ src/utils/getFetchURL.js
@@ -6,7 +6,7 @@ const MICRO_MIDIUM_API_ORIGIN = 'https://micro-medium-api-michaelhsutw.now.sh';
 const getFetchURL = R.pipe(
   R.path(['location', 'hostname']),
   R.cond([
-    [R.test(/michaelhsu.tw/), R.always(MICRO_MIDIUM_API_ORIGIN)],
+    [R.test(/michaelhsu\.tw/), R.always(MICRO_MIDIUM_API_ORIGIN)],
     [R.T, R.always(MICRO_MIDIUM_API_CORS)],
   ]),
   R.concat(R.__, '/@evenchange4/posts?limit=100&type=simple'), // eslint-disable-line
